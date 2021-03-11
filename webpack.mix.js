2f3c21001db361eea4943fabce903e47ea7c75c9
Putra Prima Arhandi <putraprima@gmail.com>
2021-03-11 09:55:39 +0700

    fixing webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,12 +11,15 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/app.css",
-    "public/css",
-    [
-        //
-    ]
+mix.js("resources/js/app.js", "public/js").sass(
+    "resources/css/app.scss",
+    "public/css"
 );
+// .postCss(
+//     "resources/css/app.css",
+//     "public/css",
+//     [
+//         //
+//     ]
 
 mix.browserSync("127.0.0.1:8000");
